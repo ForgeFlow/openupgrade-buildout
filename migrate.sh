49875ae765a3e6a484e4dcd8da42f40eac4e979d
mreficent <miquel.raich@eficent.com>
2017-05-24 17:30:07 +0200

    Minor fixes

--- migrate.sh
+++ migrate.sh
@@ -11,7 +11,7 @@ python ./pre-migration.py --db_name=$DATABASE --db_user=odoo
 
 VERSIONS=$(eval echo bin/start_openupgrade*)
 for version in $VERSIONS; do
-    $version --db_user odoo -i ao_profile --update=all --stop-after-init --database=$DATABASE || exit 1
+    $version --db_user odoo --update=all --stop-after-init --database=$DATABASE || exit 1
 done
 
 python ./post-migration.py --db_name=$DATABASE --db_user=odoo
